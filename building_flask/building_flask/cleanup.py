bd03d55ba4adff445d241fb3532e2a5eba5ae5fd
Troy <twc17@pitt.edu>
2018-05-05 10:25:49 -0400

    Ready to test cleanup db script

--- building_flask/building_flask/cleanup.py
+++ building_flask/building_flask/cleanup.py
@@ -10,6 +10,48 @@
 
 # Imports
 from main import *
-import datetime
-import redis
 
+# Number of minutes before user will be logged out
+# Default is 12 hours (720 min)
+TIMEOUT = 720
+
+# Timestamp format that is being used by program
+FORMAT = '%Y-%m-%d %H:%M:%S'
+
+def compare_time(t1, t2):
+    """Compare two timestamps, return difference in minutes
+
+    Arguments:
+        t1 -- First time stamp, as string
+        t2 -- Second time stamp, as string
+
+    Returns:
+        minutes -- Difference between two times, in minutes
+    """
+    compare1 = datetime.datetime.strptime(t1, FORMAT)
+    compare2 = datetime.datetime.strptime(t2, FORMAT)
+
+    if compare1 > compare2:
+        diff = compare1 - compare2
+    else:
+        diff = compare2 - compare1
+
+    return int(round(diff.total_seconds() / 60))
+
+
+def main():
+    now = datetime.datetime.now().strftime(FORMAT)
+
+    for key in db.keys():
+        key = key.decode('utf-8')
+        data = db.lrange(key, 0, -1)
+        bytedata = b','.join(data)
+        user_line = key + "," + bytedata.decode('utf-8')
+
+        if compare_time(now, user_line[3]) > TIMEOUT:
+            del_log(user_line[0], db)
+            user_line.append("OUT (AUTO)")
+            write_log(user_line, log_file)
+
+if __name__ == "__main__":
+    main()
